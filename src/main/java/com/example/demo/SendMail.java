711c2ec9f6aa15d4f10fd7b94e7d3169db96f0e0
greendb16 <greendb16@gmail.com>
2019-08-06 12:58:53 -0400

    final commit

--- src/main/java/com/example/demo/SendMail.java
+++ src/main/java/com/example/demo/SendMail.java
@@ -3,6 +3,6 @@ package com.example.demo;
 
 public class SendMail {
     public static void main(){
-        Mailer.send("interviewdandaman@gmail.com","RoboInterview","greendb16@gmail.com","confirmation","We have recieved your interview and will be in contact.");
+        Mailer.send("interviewdandaman@gmail.com","RoboProject","greendb16@gmail.com","confirmation","We have recieved your interview and will be in contact.");
     }
 }
